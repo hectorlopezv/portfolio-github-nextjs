038a5cd646639118d1529ed4373fb26002262e63
Hector Vinicio Lopez Molinares <hectorvmlopez@gmail.com>
2021-11-28 11:32:28 -0500

    updating api endpoint

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home(props) {
 export const getStaticProps = async (ctx) => {
   const getEnv = (route) => {
     return process.env.NODE_ENV === "production"
-      ? "https://portfolio-github-nextjs.vercel.app/"
+      ? `https://portfolio-github-nextjs.vercel.app/api/${route}`
       : `http://localhost:3000/api/${route}`;
   };
   const urls = [
